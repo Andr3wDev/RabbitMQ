163d290e85373ff307bc0e92786403ca3091ffa8
Andr3wDev <29abrown@gmail.com>
2022-02-12 15:18:48 +1300

    added ticket messaging demo

---
+++ TicketProcessor.Microservice/Consumers/TicketConsumer.cs
@@ -0,0 +1,21 @@
+﻿using MassTransit;
+using Shared.Models;
+using System.Threading.Tasks;
+
+namespace TicketProcessor.Microservice.Consumers
+{
+    public class TicketConsumer : IConsumer<Ticket>
+    {
+        // Simulates a ticket consumer
+        public async Task Consume(ConsumeContext<Ticket> context)
+        {
+            var data = context.Message;
+
+            // 1 - Validate received Ticket
+
+            // 2 - Add to Database
+
+            // 3 - Notify the User
+        }
+    }
+}
